47739a87b937d55ffd45ba2528f919197f5ce51e
Daehyeon Shin <dahini@gmail.com>
2011-04-30 19:15:41 +0900

    no big deal

--- src/main/java/com/dasolute/universe/web/DomainController.java
+++ src/main/java/com/dasolute/universe/web/DomainController.java
@@ -1,10 +1,27 @@
 package com.dasolute.universe.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dasolute.universe.domain.Document;
 
 @Controller
+@RequestMapping("/domains")
 public class DomainController {
-	
-	
+
+	@RequestMapping(value = "/{domainName}/{id}", method = RequestMethod.GET)
+	public String show(@PathVariable("domainName") String domainName, @PathVariable("id") Long id, ModelMap modelMap) {
+		if (id == null)
+			throw new IllegalArgumentException("An Identifier is required");
+
+		modelMap.addAttribute(domainName, Document.findDocument(id));
+
+		System.out.println("domainName:" + domainName + ", id:" + id);
+		//logger.dd
+		return domainName + "/show";
+	}
 
 }
